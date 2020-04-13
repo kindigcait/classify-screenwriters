9b603aef649a09ec15eb7004bf777daa3e11d2da
Nathan Drezner <nathan@drezner.com>
2020-04-13 16:06:37 -0400

    First draft and final visuals

--- classify_scripts.py
+++ classify_scripts.py
@@ -6,8 +6,9 @@ from sklearn.model_selection import GridSearchCV
 
 # Models
 # from sklearn.ensemble import RandomForestClassifier
-# from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import SGDClassifier
 # from sklearn.svm import LinearSVC
 # from sklearn.ensemble import AdaBoostClassifier
 # from sklearn.model_selection import GridSearchCV
@@ -34,45 +35,63 @@ def test_model(clf, train_data, train_target, test_data, test_target, tuned_para
 		('clf', clf),
 	])
 	
-	gs = GridSearchCV(text_clf, tuned_parameters, n_jobs = -1, cv=5, refit=True, return_train_score=True)
+
+	parameters = {
+		'vect__ngram_range': [(1, 1), (1, 2)],
+		'tfidf__use_idf': (True, False),
+		'clf__alpha': (1e-2, 1e-3),
+	}
+
+	gs = GridSearchCV(text_clf, parameters, n_jobs = -1, cv=5, refit=True, return_train_score=True)
 	
 	gs.fit(train_data, train_target)
 
 	return gs.predict(test_data), gs.classes_
 
 
-def generate_confusion(labels, predictor, y_test, y_pred):
+def generate_confusion(labels, predictor, acc, y_test, y_pred):
 	conf_mat = confusion_matrix(y_test, y_pred)
 	fig, ax = plt.subplots(figsize=(10,10))
-	sns.heatmap(conf_mat, xticklabels=labels, yticklabels=labels, annot=True, fmt='d',)
+	p = sns.heatmap(conf_mat, xticklabels=labels, yticklabels=labels, annot=True, fmt='d',)
 	plt.ylabel('Actual')
 	plt.xlabel('Predicted')
 	plt.yticks(rotation=0)
-	plt.title('Actual and predicted '+predictor+' for screenplay snippets')
+	plt.title('Actual and predicted '+predictor+' for screenplay snippets \nAccuracy: ' + str(acc) + ", sample count: " + str(len(y_pred)*4))
 
 	bottom, top = ax.get_ylim()
 	ax.set_ylim(bottom + 0.5, top - 0.5)
 
-	plt.show()
+	plt.tight_layout()
+
+	plt.savefig('visuals/w_pixar/'+predictor+"")
 
 
 # Predict the screenwriter of a script!
-def predict_scripts():
-	predictor = 'genre1'
-	df = pd.read_csv('metadata_split_80.csv')
+def predict_scripts(predictor, no_pixar=True):
+	if no_pixar:
+		df = pd.read_csv('metadata/metadata_split_no_pixar.csv')
+	else:
+		df = pd.read_csv('metadata/metadata_split.csv')
+
 
 	df = df.dropna(subset=[predictor])
-	
+
+	g = df.groupby(predictor)
+	df = pd.DataFrame(g.apply(lambda x: x.sample(g.size().min()).reset_index(drop=True)))	
+
 	X = df['text']
 	y = df[predictor]
 
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-	y_pred, classes = test_model(DecisionTreeClassifier(), X_train, y_train, X_test, y_test)
+	y_pred, classes = test_model(SGDClassifier(), X_train, y_train, X_test, y_test)
 
-	print(accuracy_score(y_test, y_pred))
-	generate_confusion(classes, predictor, y_test, y_pred)
+	acc = round(accuracy_score(y_test, y_pred), 3)
+	generate_confusion(classes, predictor, acc, y_test, y_pred)
 
 
-predict_scripts()
+predictors = ['title', 'screenwriter', 'production_company','genre1','genre2']
+for predictor in predictors:
+	print(predictor)
+	predict_scripts(predictor, no_pixar=False)
 
--- meta_csv.py
+++ meta_csv.py
@@ -1,14 +1,23 @@
 import os
 import re
 import pandas as pd
+import nltk
+from nltk.corpus import stopwords
 
+stopwords = set(stopwords.words('english'))
 
 def clean_text(text):
+	
+
 	text = re.sub(r'\b[A-Z]+\b', '', text)
 	# Remove line breaks
 	text = re.sub('\n', ' ', text)
 	# Remove excess spaces
 	text = re.sub(' +', ' ', text)
+
+	text = set(text.split())
+
+	text = list(text - stopwords)
 
 	return text
 
@@ -27,12 +36,14 @@ for screenwriter in screenwriters:
 		f = open(base+"/"+title, 'r')
 		genres = f.readline().strip()
 		director = f.readline().strip()
+		production_company = f.readline().strip()
 		genres = genres.split(',')
 
 		cur = {}
 		cur['title'] = title.split('.')[0]
 		cur['screenwriter'] = screenwriter
 		cur['director'] = director
+		cur['production_company'] = production_company
 
 		for i in range(0, len(genre_tags)):
 			try:
@@ -41,8 +52,7 @@ for screenwriter in screenwriters:
 				cur[genre_tags[i]] = None
 
 		script = clean_text(f.read())
-		script = script.split(' ')
-		
+
 		for i in range(0, len(script), chunk_length):
 			temp = dict(cur)
 			text = ' '.join(script[i:i+chunk_length])
@@ -51,6 +61,6 @@ for screenwriter in screenwriters:
 
 df = pd.DataFrame(meta)
 print(df)
-df.to_csv('metadata_split_80.csv')
+df.to_csv('metadata_split.csv')
 
 
