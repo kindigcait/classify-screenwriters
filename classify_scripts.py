153f2f188c717a0ba249ba7b54585fb51ab19dfd
Nathan Drezner <nathan@drezner.com>
2020-04-13 14:19:04 -0400

    Update with more testable attributes and metadata

--- classify_scripts.py
+++ classify_scripts.py
@@ -1,6 +1,3 @@
-import os
-import re
-
 # Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
@@ -8,57 +5,24 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 
 # Models
-from sklearn.linear_model import LogisticRegression
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+# from sklearn.svm import LinearSVC
+# from sklearn.ensemble import AdaBoostClassifier
+# from sklearn.model_selection import GridSearchCV
+
 
 from sklearn.model_selection import train_test_split
 
 import numpy as np
+import pandas as pd
 
 # Graphing library
 import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
-
-# Import the text files as a set of screenwriter, script dict items
-def get_texts(remove_stop=False):
-	sws = [folder for folder in os.listdir('data') if not folder.startswith('.') ]
-
-	sw_scripts = {}
-	
-	for sw in sws:
-		base = 'data/'+sw
-		# Read in the raw files
-		scripts = [open(base+"/"+file, 'r').read() for file in os.listdir(base) if not file.startswith('.')]
-		# Remove character names, settings, etc that are listed in all caps
-		scripts = [re.sub(r'\b[A-Z]+\b', '', script) for script in scripts]
-		# Remove line breaks
-		scripts = [re.sub('\n', ' ', script) for script in scripts]
-		# Remove excess spaces
-		scripts = [re.sub(' +', ' ', script) for script in scripts]
-		# Assign the script to the 
-		sw_scripts[sw.split('.')[0]] = scripts
-	
-	return sw_scripts
-
-
-# Convert the imported screenplays into usable data
-# Change the chunk length to get a different sized word byte for training
-def get_data(chunk_length=80):
-	sws = get_texts()
-	data = []
-	target = []
-
-	n = 0
-	for sw in sws:
-		for script in sws[sw]:
-			cur = script.split(' ')
-			# break the text into 80-word chunks
-			chunks = [' '.join(cur[x:x+chunk_length]) for x in range(0, len(cur), chunk_length)]
-			data += chunks
-			target += [n]*len(chunks)
-		n+=1
-
-	return data, target
+from sklearn.metrics import accuracy_score
 
 
 # Generalized tester function
@@ -74,26 +38,17 @@ def test_model(clf, train_data, train_target, test_data, test_target, tuned_para
 	
 	gs.fit(train_data, train_target)
 
-	results = gs.cv_results_
-
-	predicted_test = gs.predict(test_data)
-	avg_test = np.mean(predicted_test == test_target)
-
-	predicted_train = gs.predict(train_data)
-	avg_train = np.mean(predicted_train == train_target)
-
-	return predicted_test
+	return gs.predict(test_data), gs.classes_
 
 
-def generate_confusion(y_test, y_pred):
-	names = [name for name in get_texts()]
-
+def generate_confusion(labels, predictor, y_test, y_pred):
 	conf_mat = confusion_matrix(y_test, y_pred)
 	fig, ax = plt.subplots(figsize=(10,10))
-	sns.heatmap(conf_mat, xticklabels=names, yticklabels=names, annot=True, fmt='d',)
+	sns.heatmap(conf_mat, xticklabels=labels, yticklabels=labels, annot=True, fmt='d',)
 	plt.ylabel('Actual')
 	plt.xlabel('Predicted')
-	plt.title('Actual and predicted screenwriters for screenplay snippets')
+	plt.yticks(rotation=0)
+	plt.title('Actual and predicted '+predictor+' for screenplay snippets')
 
 	bottom, top = ax.get_ylim()
 	ax.set_ylim(bottom + 0.5, top - 0.5)
@@ -103,13 +58,20 @@ def generate_confusion(y_test, y_pred):
 
 # Predict the screenwriter of a script!
 def predict_scripts():
-	X, y = get_data()
+	predictor = 'genre1'
+	df = pd.read_csv('metadata_split_80.csv')
 
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+	df = df.dropna(subset=[predictor])
+	
+	X = df['text']
+	y = df[predictor]
 
-	y_pred = test_model(LogisticRegression(), X_train, y_train, X_test, y_test)
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
 
-	generate_confusion(y_test, y_pred)
+	y_pred, classes = test_model(DecisionTreeClassifier(), X_train, y_train, X_test, y_test)
+
+	print(accuracy_score(y_test, y_pred))
+	generate_confusion(classes, predictor, y_test, y_pred)
 
 
 predict_scripts()
---
+++ meta_csv.py
@@ -0,0 +1,56 @@
+import os
+import re
+import pandas as pd
+
+
+def clean_text(text):
+	text = re.sub(r'\b[A-Z]+\b', '', text)
+	# Remove line breaks
+	text = re.sub('\n', ' ', text)
+	# Remove excess spaces
+	text = re.sub(' +', ' ', text)
+
+	return text
+
+chunk_length = 80
+genre_tags = ['genre1', 'genre2', 'genre3', 'genre4']
+screenwriters = [folder for folder in os.listdir('data') if not folder.startswith('.') ]
+
+meta = []
+for screenwriter in screenwriters:
+	base = 'data/'+screenwriter
+	# Get the titles
+	titles = [file for file in os.listdir(base) if not file.startswith('.')]
+
+
+	for title in titles:
+		f = open(base+"/"+title, 'r')
+		genres = f.readline().strip()
+		director = f.readline().strip()
+		genres = genres.split(',')
+
+		cur = {}
+		cur['title'] = title.split('.')[0]
+		cur['screenwriter'] = screenwriter
+		cur['director'] = director
+
+		for i in range(0, len(genre_tags)):
+			try:
+				cur[genre_tags[i]] = genres[i]
+			except:
+				cur[genre_tags[i]] = None
+
+		script = clean_text(f.read())
+		script = script.split(' ')
+		
+		for i in range(0, len(script), chunk_length):
+			temp = dict(cur)
+			text = ' '.join(script[i:i+chunk_length])
+			temp['text'] = text
+			meta.append(temp)
+
+df = pd.DataFrame(meta)
+print(df)
+df.to_csv('metadata_split_80.csv')
+
+
